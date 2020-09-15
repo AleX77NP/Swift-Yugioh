752595b66cd9ab67d34480f72b21acfd0291b34a
Aleksandar Milanovic <milanovicaleX77@gmail.com>
2020-09-15 13:39:46 +0200

    dodao Button, ikonice, promenu na klik...

--- Yugioh/DuelistView.swift
+++ Yugioh/DuelistView.swift
@@ -13,21 +13,38 @@ struct DuelistView: View {
     
     var duelist: Duelist
     
+    @State var isFav: Bool = false
+    
     var body: some View {
         ScrollView {
-        VStack {
+            VStack(alignment: .center) {
+            Text(duelist.name).font(.largeTitle)
+          //  Divider()
             Image(duelist.img)
             .resizable()
                 .renderingMode(.original)
                 .aspectRatio(contentMode: .fit)
-                .frame(width:180, height: 180)
-                Text(duelist.name).font(.headline)
+                .frame(width:200, height: 200)
                 Text("Age: " + String(duelist.age)).font(.subheadline)
                 Text("Deck: " + duelist.deck).font(.subheadline)
                 Text("Ace card: " + duelist.ace).font(.subheadline)
+               Divider()
                 Text("Bio: ").font(.subheadline)
                 Text(duelist.biography)
                     .padding(.horizontal, 20)
+                Divider()
+                HStack {
+                    Text("Add to favorites? ")
+                    Button(action: {
+                        print("clicked")
+                        self.isFav = !self.isFav
+                    }) {
+                        Image(systemName: !self.isFav ? "heart" : "trash")
+                        .resizable()
+                            .foregroundColor(.red)
+                            .frame(width: 20, height: 20)
+                    }
+                }
         }
      }
   }
